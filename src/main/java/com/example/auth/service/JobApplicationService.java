225876bf783c1e77c1d8ca6999e076c8b01cd064
Shivansh6317 <shivanxhskumar@gmail.com>
2025-11-16 19:37:20 +0530

    re apply alowed if withdrawn

--- src/main/java/com/example/auth/service/JobApplicationService.java
+++ src/main/java/com/example/auth/service/JobApplicationService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -53,7 +54,16 @@ public class JobApplicationService {
         if (applicationRepository.existsByJobPostAndApplicant(jobPost, profile)) {
             throw new CustomException("You have already applied to this job", HttpStatus.CONFLICT);
         }
+        Optional<JobApplication> existing =
+                applicationRepository.findByJobPostAndApplicant(jobPost, profile);
 
+        if (existing.isPresent()) {
+            JobApplication previous = existing.get();
+
+            if (previous.getStatus() != ApplicationStatus.WITHDRAWN) {
+                throw new CustomException("You have already applied to this job", HttpStatus.CONFLICT);
+            }
+        }
         JobApplication application = JobApplication.builder()
                 .jobPost(jobPost)
                 .applicant(profile)
