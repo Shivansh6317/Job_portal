45428b1fb4115f55440b72116edb553041c76650
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2025-11-16 20:48:52 +0530

    creating repository for chatroom

---
+++ src/main/java/com/example/auth/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.example.auth.repository.
+
+import com.example.auth.entity.ChatRoom;
+import com.example.auth.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByChatRoomOrderBySentAtAsc(ChatRoom chatRoom);
+}
